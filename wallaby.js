4700f16549142ef91aa274f96cb0a59590d02740
Tongtwist <arnaud.dussart@gmail.com>
2023-03-13 23:46:45 +0100

    resetup repo

--- wallaby.js
+++ wallaby.js
@@ -1,6 +1,6 @@
 module.exports = () => ({
 	name: "result-js",
-	localProjectDir: "workspaces/JS/result-js/",
+	localProjectDir: "workspaces/result-js/",
 	autoDetect: true,
 	files: ["src/**/*.ts", {pattern: "src/**/*.spec.ts", ignore: true}],
 	tests: ["src/**/*.spec.ts"],
