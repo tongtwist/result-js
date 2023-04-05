8d43d2033cb4c2fa906230824d38065a8572c6df
Tongtwist <arnaud.dussart@gmail.com>
2023-04-05 11:19:54 +0200

    pre-bundle

--- src/Result.spec.ts
+++ src/Result.spec.ts
@@ -1,17 +1,55 @@
 import {z} from "zod"
-import {Result, resultSchema} from "./Result"
+import {SuccessResult, FailureResult, Result} from "./Result"
 
 /**
  * Shape spec
  */
-export interface IResult<R> {
-	readonly error?: Error
-	readonly value?: R
-	readonly isOk: boolean
-	readonly isSuccess: boolean
-	readonly isError: boolean
-	readonly isFailure: boolean
+
+/**
+ * The "success" specific case of the TResultSchema pseudo-type
+ */
+export type TSuccessResultSchema<R, RS extends z.ZodType<R> = z.ZodType<R>> = z.infer<
+	ReturnType<typeof SuccessResult.serializedSchema<R, RS>>
+>
+
+/**
+ * The "success" specific case of the IResult pseudo-interface
+ *
+ * @export
+ * @interface ISuccessResult
+ * @template R
+ */
+export interface ISuccessResult<R> {
+	readonly value: R
+	readonly isOk: true
+	readonly isSuccess: true
+	readonly isError: false
+	readonly isFailure: false
 }
+
+/**
+ * The "failure" specific case of the TResultSchema pseudo-type
+ */
+export type TFailureResultSchema = z.infer<typeof FailureResult.serializedSchema>
+
+/**
+ * The "failure" specific case of the IResult pseudo-interface
+ *
+ * @export
+ * @interface IFailureResult
+ */
+export interface IFailureResult {
+	readonly error: Error
+	readonly isOk: false
+	readonly isSuccess: false
+	readonly isError: true
+	readonly isFailure: true
+}
+
+/**
+ * The pseudo-interface for a result of type `R` (or an Error)
+ */
+export type IResult<R> = ISuccessResult<R> | IFailureResult
 
 /**
  * Logic spec
@@ -20,62 +58,55 @@ describe("In Result.ts", () => {
 	describe("Result class", () => {
 		it("should create a correct failure result object, when given an error message string", () => {
 			const msgError = "bam!"
-			const r: Result = Result.fail(msgError)
-			expect(r).toBeInstanceOf(Result)
-			expect(r.isError).toBe(true)
-			expect(r.isFailure).toBe(true)
+			const r: IResult<any> = Result.fail(msgError)
+			expect(r).toBeInstanceOf(FailureResult)
 			expect(r.isOk).toBe(false)
 			expect(r.isSuccess).toBe(false)
-			expect(r.value).toBeUndefined()
-			expect(r.error).toBeInstanceOf(Error)
-			if (r.error) {
-				expect(r.error.message).toBe(msgError)
+			expect(r.isError).toBe(true)
+			expect(r.isFailure).toBe(true)
+			expect(Result.isOk(r)).toBe(false)
+			expect(Result.isError(r)).toBe(true)
+			if (Result.isError(r)) {
+				// @ts-ignore
+				expect(r.value).toBeUndefined()
+				expect(r.error).toBeInstanceOf(Error)
+				expect(r.error.message).toEqual(msgError)
 			}
 		})
 		it("should create a correct failure result object, when given an error object", () => {
 			const msgError = "bam!"
 			const err = new Error(msgError)
-			const r: Result = Result.fail(err)
-			expect(r).toBeInstanceOf(Result)
+			const r: IResult<any> = Result.fail(err)
+			expect(r).toBeInstanceOf(FailureResult)
 			expect(r.isError).toBe(true)
 			expect(r.isFailure).toBe(true)
 			expect(r.isOk).toBe(false)
 			expect(r.isSuccess).toBe(false)
-			expect(r.value).toBeUndefined()
-			expect(r.error).toBeInstanceOf(Error)
-			if (r.error) {
-				expect(r.error).toBe(err)
-				expect(r.error.message).toBe(msgError)
+			expect(Result.isOk(r)).toBe(false)
+			expect(Result.isError(r)).toBe(true)
+			if (Result.isError(r)) {
+				// @ts-ignore
+				expect(r.value).toBeUndefined()
+				expect(r.error).toBeInstanceOf(Error)
+				expect(r.error).toEqual(err)
+				expect(r.error.message).toEqual(msgError)
 			}
 		})
 		it("should create a correct success result object", () => {
 			const value = "foo"
-			const r: Result = Result.success<string>(value)
-			expect(r).toBeInstanceOf(Result)
+			const r: IResult<string> = Result.success<string>(value)
+			expect(r).toBeInstanceOf(SuccessResult)
 			expect(r.isError).toBe(false)
 			expect(r.isFailure).toBe(false)
 			expect(r.isOk).toBe(true)
 			expect(r.isSuccess).toBe(true)
-			expect(r.value).toBe(value)
-			expect(r.error).toBeUndefined()
-		})
-	})
-	describe("resultSchema", () => {
-		it("should return a schema matching a Result object", () => {
-			const rString: Result = Result.success<string>("foo")
-			const rNumber: Result = Result.success<number>(123)
-			const schemString = resultSchema(z.string())
-			const schemNumber = resultSchema(z.number())
-			const schemUnknown = resultSchema()
-			expect(schemString.safeParse({}).success).toBe(false)
-			expect(schemNumber.safeParse({}).success).toBe(false)
-			expect(schemUnknown.safeParse({}).success).toBe(false)
-			expect(schemString.safeParse(rString).success).toBe(true)
-			expect(schemNumber.safeParse(rString).success).toBe(false)
-			expect(schemUnknown.safeParse(rString).success).toBe(true)
-			expect(schemString.safeParse(rNumber).success).toBe(false)
-			expect(schemNumber.safeParse(rNumber).success).toBe(true)
-			expect(schemUnknown.safeParse(rNumber).success).toBe(true)
+			expect(Result.isOk(r)).toBe(true)
+			expect(Result.isError(r)).toBe(false)
+			if (Result.isOk(r)) {
+				expect(r.value).toEqual(value)
+				// @ts-ignore
+				expect(r.error).toBeUndefined()
+			}
 		})
 	})
 })
